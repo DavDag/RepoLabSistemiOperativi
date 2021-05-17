8b2886d5f61f8ba54d73d75effbc4ea25dcb313e
DavDag <davdag24@gmail.com>
2021-05-17 17:40:17 +0200

    Setup test for compression

---
+++ test-compression/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static size_t compress(char* buffer, size_t size) {
+    size_t new_size = size;
+    return new_size;
+}
+
+static size_t decompress(char* buffer, size_t size) {
+    size_t new_size = size;
+    return new_size;
+}
+
+int main(int argc, char** argv) {
+
+    return EXIT_SUCCESS;
+}
