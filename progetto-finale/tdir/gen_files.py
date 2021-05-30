5df05717f37ab1dbb2e8053322ae553a28d9b089
DavDag <davdag24@gmail.com>
2021-05-30 15:57:55 +0200

    Added file generation. Added rule to makefile.

--- progetto-finale/tdir/gen_files.py
+++ progetto-finale/tdir/gen_files.py
@@ -1,7 +1,14 @@
 import random as rm
 import os
+import sys
 
-path = os.getcwd()
+numFiles  = int([arg.split("=", 1)[1] for arg in sys.argv if arg.startswith("-numFiles=")][0]) 
+minSizeKb = int([arg.split("=", 1)[1] for arg in sys.argv if arg.startswith("-minSizeKb=")][0]) 
+maxSizeKb = int([arg.split("=", 1)[1] for arg in sys.argv if arg.startswith("-maxSizeKb=")][0]) 
+outDir    = [arg.split("=", 1)[1] for arg in sys.argv if arg.startswith("-outDir=")][0]
+
+os.mkdir(outDir)
+path = os.path.join(os.getcwd(), outDir)
 
 def conv_bytes(size):
     if size < 1024:          return str(size) + "B"
@@ -9,13 +16,13 @@ def conv_bytes(size):
     else:                    return "{:4.2f} MB".format(size / 1204 / 1024)
 
 def gen_file(name, values):
-    size = 512 * 1024 * rm.randint(1, 8) # 512kb ~> 4Mb
+    size = 1024 * rm.randint(1, 64)
     with open(name, "wb") as f:
         for i in range(0, size):
             f.write(rm.choice(values))
     return size
 
-for i in range(0, 16):
+for i in range(0, 64):
     name = path + "/file{:02d}.txt".format(i)
     values = [chr(rm.randint(0, 127)).encode('utf-8') for j in range(0, 8 * rm.randint(8, 16))]
     size = gen_file(name, values)
