e7dffbb10da2c9ee6805000e6490cce545d0849c
DavDag <davdag24@gmail.com>
2021-05-30 15:27:39 +0200

    Added gen_files.py scripts

--- progetto-finale/tdir/longdir/gen_files.py
+++ progetto-finale/tdir/longdir/gen_files.py
@@ -9,13 +9,13 @@ def conv_bytes(size):
     else:                    return "{:4.2f} MB".format(size / 1204 / 1024)
 
 def gen_file(name, values):
-    size = 512 * 1024 * rm.randint(1, 8) # 512kb ~> 4Mb
+    size = 1024 * rm.randint(1, 64) # 1kb ~> 64kb
     with open(name, "wb") as f:
         for i in range(0, size):
             f.write(rm.choice(values))
     return size
 
-for i in range(0, 16):
+for i in range(0, 64):
     name = path + "/file{:02d}.txt".format(i)
     values = [chr(rm.randint(0, 127)).encode('utf-8') for j in range(0, 8 * rm.randint(8, 16))]
     size = gen_file(name, values)
