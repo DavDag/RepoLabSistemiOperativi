e44ce53cf591cd1d28747a88a2566a2079f564fb
DavDag <davdag24@gmail.com>
2021-05-31 18:25:40 +0200

    Updated test2

--- progetto-finale/scripts/test2.sh
+++ progetto-finale/scripts/test2.sh
@@ -5,7 +5,7 @@ client="$1 $prefix"
 idir="$(pwd)/tdir"
 
 $client -w $idir/longdir,n=16 -D ./out/1
-$client -W $idir/bigdir/file00.txt -D ./out/2
-$client -W $idir/file2.txt,$idir/smallfile1.txt -D ./out/3
+$client -W $idir/file2.txt -D ./out/2
+$client -W $idir/smallfile1.txt,$idir/smallfile2.txt -D ./out/3
 
 wait
